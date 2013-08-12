016a05c9f9f3fbd73c38c4727743813a2816e5fa
Christine Coulter <christinetcoulter@gmail.com>
2013-08-12 14:10:58 -0400

    Initial commit. Add root to routes. Create main_controller.rb file. Add index action.

---
+++ banque_app/app/controllers/main_controller.rb
@@ -0,0 +1,6 @@
+class MainController < ActionController::Base
+
+  def index
+  end
+
+end
---
+++ banque_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BanqueApp::Application.config.secret_token = '49af44a524e3225670ede1448b45e8df41d87548e7dbdbe8511ce02534077e1d2fc162d51da2c4a9c1a5dbc94e4b2708d38e3c00b263b0ca980a8afac6579c3e'
